4cdc08e58b2061ccf0f41d52c51c97f2d62f799d
Thad House <thadhouse1@gmail.com>
2016-08-31 18:59:54 -0700

    Adds fast bitconverter code
    Previously always involved allocations, which was slow and had a lot of
    memory allocations. New code uses unsafe code, but is MUCH faster.

---
+++ src/Shared/Wire/FastBitConverterBE.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace NetworkTables.Wire
+{
+    internal class FastBitConverterBE : IFastBitConverter
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddUShort(List<byte> list, ushort val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddShort(List<byte> list, short val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddUInt(List<byte> list, uint val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+                list.Add(v[2]);
+                list.Add(v[3]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddInt(List<byte> list, int val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+                list.Add(v[2]);
+                list.Add(v[3]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddULong(List<byte> list, ulong val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+                list.Add(v[2]);
+                list.Add(v[3]);
+                list.Add(v[4]);
+                list.Add(v[5]);
+                list.Add(v[6]);
+                list.Add(v[7]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddLong(List<byte> list, long val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+                list.Add(v[2]);
+                list.Add(v[3]);
+                list.Add(v[4]);
+                list.Add(v[5]);
+                list.Add(v[6]);
+                list.Add(v[7]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddFloat(List<byte> list, float val)
+        {
+            unsafe
+            {
+                byte* v = (byte*)&val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+                list.Add(v[2]);
+                list.Add(v[3]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [System.Security.SecuritySafeCritical]
+        public void AddDouble(List<byte> list, double val)
+        {
+            unsafe
+            {
+                byte* v = (byte*) &val;
+                list.Add(v[0]);
+                list.Add(v[1]);
+                list.Add(v[2]);
+                list.Add(v[3]);
+                list.Add(v[4]);
+                list.Add(v[5]);
+                list.Add(v[6]);
+                list.Add(v[7]);
+            }
+
+        }
+    }
+}
